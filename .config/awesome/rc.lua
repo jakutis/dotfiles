71d873d9ac781b72acbbfe73de5f031675e56ef7
Vytautas Jakutis <vytautas@jakut.is>
2013-03-15 16:58:16 +0200

    .config: awesome: rc.lua: make gst-launch-0.10 window sticky and floating

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -372,8 +372,8 @@ awful.rules.rules = {
     { rule = { class = "gimp" },
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    { rule = { name = "gst-launch-0.10" },
+      properties = { sticky = true, floating = true } }
 }
 -- }}}
 
