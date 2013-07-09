5f5f83cd65248d987f02669f04bb9d2dfb823006
Vytautas Jakutis <vytautas@jakut.is>
2013-07-09 21:42:38 +0300

    .config: awesome: theme.lua: adjust wallpaper for changed storage/photos layout

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -72,7 +72,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/tahu/etc/storage/photos/2013/wallpaper/typography-world-map-4500x2498_edited.jpg"
+theme.wallpaper = "/home/tahu/etc/storage/photos/2013 - wallpaper/typography-world-map-4500x2498_edited.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
