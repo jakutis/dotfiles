63eb85beddc15fd313a6ad6f6b5732c59b2a04bf
Vytautas Jakutis <vytautas@jakut.is>
2013-11-03 02:52:29 +0200

    change wallpaper

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -72,7 +72,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/tahu/etc/storage/photos/2013 - wallpaper/peaceful-ocean_1920x1080_24593.jpg"
+theme.wallpaper = "/home/tahu/etc/storage/photos/2013 - wallpaper/blade runner - rachel.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
