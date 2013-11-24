4c9e555003c5acb0afc8fa5dc90fa3f2ca892cdf
Vytautas Jakutis <vytautas@jakut.is>
2013-11-24 23:15:14 +0200

    flexpaper: meta.js: add logging to stderr, check also css background-image for url

--- flexpaper/meta.js
+++ flexpaper/meta.js
@@ -1,7 +1,10 @@
+var sys = require('system');
 var url = require('system').args[1];
 var page = require('webpage').create();
+sys.stderr.write('Opening main web page\n');
 page.open(url, function (status) {
     var check = function() {
+        sys.stderr.write('Checking for document\n');
         var hasElement = function() {
             return page.evaluate(function() {
                 return !!document.getElementById('page_0_documentViewer');
@@ -10,6 +13,16 @@ page.open(url, function (status) {
         var getSrc = function() {
             return page.evaluate(function() {
                 return document.getElementById('page_0_documentViewer').src;
+            });
+        };
+        var getCSSSrc = function() {
+            return page.evaluate(function() {
+                var src = document.getElementById('page_0_documentViewer').style.backgroundImage;
+                if(src.substr(0, 'url('.length) === 'url(') {
+                    src = src.substr('url('.length);
+                    return src.substr(0, src.length - 1);
+                }
+                return '';
             });
         };
         var getCookie = function() {
@@ -24,8 +37,14 @@ page.open(url, function (status) {
         };
         if(hasElement()) {
             var src = getSrc();
+            sys.stderr.write('Checking for document ID\n');
             var i = src.indexOf('data=');
+            if(i < 0) {
+                src = getCSSSrc();
+                i = src.indexOf('data=');
+            }
             if(i >= 0) {
+                sys.stderr.write('Outputing metadata\n');
                 console.log(src.substr(0, i - 1));
                 src = src.substr(i);
                 console.log(src.substring('data='.length, src.indexOf('&')));
