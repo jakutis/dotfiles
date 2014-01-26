e8143fe95ac6710ade4b1ba7e2439d4d441c5c33
Vytautas Jakutis <vytautas@jakut.is>
2014-01-26 08:18:13 +0000

    move xbindkeys from .xinitrc to .config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -399,5 +399,6 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 awful.util.spawn("awesome-ip")
+awful.util.spawn("xbindkeys")
 awful.util.spawn_with_shell("wmname LG3D")
 -- }}}
