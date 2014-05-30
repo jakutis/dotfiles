6c74d925a9d83173c758b3cdec0532d0ab9cda87
Vytautas Jakutis <vytautas@jakut.is>
2014-05-30 07:49:46 +0000

    .config: awesome: rc.lua: set terminal to urxvt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -40,7 +40,7 @@ end
 beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
