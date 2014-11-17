f444880b7c60587012a67f3629329b802931e52d
Vytautas Jakutis <jakutis@wix.com>
2014-11-17 15:44:37 +0200

    .config: awesome.wix: disable clock and layout boxes

--- .config/awesome.wix/rc.lua
+++ .config/awesome.wix/rc.lua
@@ -190,8 +190,8 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
-    right_layout:add(mytextclock)
-    right_layout:add(mylayoutbox[s])
+--    right_layout:add(mytextclock)
+--    right_layout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
     local layout = wibox.layout.align.horizontal()
