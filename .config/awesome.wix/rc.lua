895f8ed097b43a17c671c3e0c87c75678576b253
Vytautas Jakutis <jakutis@wix.com>
2014-12-05 18:14:26 +0200

    .config: awesome.wix: rc.lua: add pomodoro widget

--- .config/awesome.wix/rc.lua
+++ .config/awesome.wix/rc.lua
@@ -112,6 +112,8 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- Create a textclock widget
 mytextclock = awful.widget.textclock()
 
+mypomodorowidget = wibox.widget.textbox()
+
 -- Create a wibox for each screen and add it
 mywibox = {}
 mypromptbox = {}
@@ -191,6 +193,7 @@ for s = 1, screen.count() do
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
 --    right_layout:add(mytextclock)
+    right_layout:add(mypomodorowidget)
 --    right_layout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
