8c823486cbfa44d140f52ef5ead013acac61efd7
Vytautas Jakutis <vytautas@jakut.is>
2015-01-25 20:16:43 +0000

    .config/awesome.wix: theme.lua: change location of wallpaper.png

--- .config/awesome.wix/theme.lua
+++ .config/awesome.wix/theme.lua
@@ -74,7 +74,7 @@ theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/defau
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
 -- theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
-theme.wallpaper = "/home/jakutis/etc/wallpaper.png"
+theme.wallpaper = "/home/jakutis/wallpaper.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
