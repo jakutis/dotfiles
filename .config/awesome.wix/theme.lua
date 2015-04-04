dbe168267543636ff4f53bc4e542b9c0d920dca7
Vytautas Jakutis <vytautas@jakut.is>
2015-04-04 11:45:11 +0300

    awesome: remove window border

--- .config/awesome.wix/theme.lua
+++ .config/awesome.wix/theme.lua
@@ -17,7 +17,7 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.border_width  = 1
+theme.border_width  = 0
 theme.border_normal = "#000000"
 theme.border_focus  = "#666666"
 theme.border_marked = "#999999"
