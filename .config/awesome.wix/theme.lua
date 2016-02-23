7f2034b1e42e13dc534a20dcac31f48cecdf2ad8
Vytautas Jakutis <vytautas@jakut.is>
2016-02-23 19:55:46 +0200

    .config: awesome.wix: theme.lua: re-add the taglist squares to display which tags have windows

--- .config/awesome.wix/theme.lua
+++ .config/awesome.wix/theme.lua
@@ -1,6 +1,8 @@
 theme = {}
 
 theme.font          = "monospace 8"
+theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
 
 theme.bg_normal     = "#000000"
 theme.bg_focus      = "#efefef"
