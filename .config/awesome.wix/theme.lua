6aedb5df1f9f607cfdea40b20df12fbe81fd8e14
Vytautas Jakutis <vytautas@jakut.is>
2016-03-03 17:52:38 +0200

    tweak the terminal and window manager colors to match solarized theme background

--- .config/awesome.wix/theme.lua
+++ .config/awesome.wix/theme.lua
@@ -5,14 +5,14 @@ theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squaref
 theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
 
 theme.bg_normal     = "#000000"
-theme.bg_focus      = "#efefef"
+theme.bg_focus      = "#fdf6e3"
 theme.bg_urgent     = "#000000"
 theme.bg_minimize   = "#000000"
 
-theme.fg_normal     = "#efefef"
+theme.fg_normal     = "#ffffff"
 theme.fg_focus      = "#000000"
-theme.fg_urgent     = "#efefef"
-theme.fg_minimize   = "#efefef"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = "0"
 theme.border_normal = "#000000"
