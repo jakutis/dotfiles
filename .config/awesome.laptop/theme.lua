6b07bbf786ad9881b1166da5f492e4fa3c51a523
Vytautas Jakutis <vytautas@jakut.is>
2018-10-23 08:19:37 +0300

    .config: awesome.laptop: make the toolbar real black

--- .config/awesome.laptop/theme.lua
+++ .config/awesome.laptop/theme.lua
@@ -6,8 +6,8 @@ local theme = {}
 
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_normal     = "#000000"
+theme.bg_focus      = "#666666"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
