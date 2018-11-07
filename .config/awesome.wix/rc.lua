96d8009a9d3f6677e60e4ab271a5826cff6b1245
Vytautas Jakutis <vytautas@jakut.is>
2018-11-07 15:40:51 +0200

    .config: awesome.wix: switch terminal from konsole to xterm

--- .config/awesome.wix/rc.lua
+++ .config/awesome.wix/rc.lua
@@ -44,7 +44,7 @@ end
 beautiful.init("/home/jakutis/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "konsole"
+terminal = "xterm"
 editor = os.getenv("EDITOR") or "vi"
 editor_cmd = terminal .. " -e " .. editor
 
